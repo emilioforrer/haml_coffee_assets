91a218dc3313f0926f406eb1e37b8bf63010bfd1
Jimmy Cuadra <jimmy@jimmycuadra.com>
2013-01-10 16:21:28 -0800

    Include the real hamlc helpers when evaluating server-side templates.

--- lib/haml_coffee_assets/action_view/template_handler.rb
+++ lib/haml_coffee_assets/action_view/template_handler.rb
@@ -4,26 +4,9 @@ module HamlCoffeeAssets
       def self.call(template)
         <<-RUBY
           jst = ::HamlCoffeeAssets::Compiler.compile("", #{template.source.inspect}, false)
-          context = ExecJS.compile("var window = {}, jst = \#{jst} #{hamlcoffee_source}")
-          context.eval("jst(\#{local_assigns.to_json})").html_safe
+          context = ExecJS.compile(%{var window = {}, jst = \#{jst}} + ::HamlCoffeeAssets.helpers)
+          context.eval(%{jst(\#{local_assigns.to_json})}).html_safe
         RUBY
-      end
-
-      private
-
-      def self.hamlcoffee_source
-        <<-JS
-          window.HAML = {
-            escape: function (text) { return text; },
-            cleanValue: function (text) { return text; },
-            context: function (locals) { return locals; },
-            preserve: function (text) { return text; },
-            findAndPreserve: function (text) { return text; },
-            surround: function () {},
-            succeed: function () {},
-            proceed: function () {}
-          };
-        JS
       end
     end
   end
