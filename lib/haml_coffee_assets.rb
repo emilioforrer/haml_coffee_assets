ac291d54c4f8bf8a23904e74f7a8247f3fad7e60
Dane O'Connor <dane.oconnor@gmail.com>
2013-04-12 12:25:18 -0400

    Monkey patch ActionView::Template so it doesn't swallow ExecJS exceptions

--- lib/haml_coffee_assets.rb
+++ lib/haml_coffee_assets.rb
@@ -16,6 +16,7 @@ require 'haml_coffee_assets/tilt/template_handler'
 if defined?(Rails)
   require 'rails'
   require 'haml_coffee_assets/rails/engine'
+  require 'haml_coffee_assets/action_view/patches'
 else
   require 'sprockets/engines'
   Sprockets.register_engine '.hamlc', ::HamlCoffeeAssets::Tilt::TemplateHandler
