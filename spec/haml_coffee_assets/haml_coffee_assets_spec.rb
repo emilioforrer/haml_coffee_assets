3b043a7ec702a7d7422e3ccb9e2392cf806bf380
Dane O'Connor <dane.oconnor@gmail.com>
2013-04-13 14:01:12 -0400

    HamlCoffeeAssets#helpers returns global context (for back compatibility)

--- spec/haml_coffee_assets/haml_coffee_assets_spec.rb
+++ spec/haml_coffee_assets/haml_coffee_assets_spec.rb
@@ -6,4 +6,12 @@ describe HamlCoffeeAssets do
       HamlCoffeeAssets.config.should be_an_instance_of ::HamlCoffeeAssets::Configuration
     end
   end
+
+  describe '.helpers' do
+    it "returns the global context from asset pipeline" do
+      HamlCoffeeAssets::GlobalContext.stub(:to_s) { "foo" }
+      helpers = HamlCoffeeAssets.helpers
+      helpers.should =~ /foo/
+    end
+  end
 end
