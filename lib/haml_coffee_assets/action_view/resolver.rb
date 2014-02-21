8f472ab078b78e41db08d754cabf5ddb598d8ae1
Kevin Tham <kevinsf90@gmail.com>
2014-02-20 16:40:37 -0800

    Fix compatibility with Rails 4.1.0.rc1 by forward-porting a require statement to load FileSystemResolver * This fixes haml_coffee_assets-1.16.0/lib/haml_coffee_assets/action_view/resolver.rb:9:in `<module:ActionView>': uninitialized constant ActionView::FileSystemResolver (NameError) * For more details, see rails @ 3fbff7811bc7142e6f4142f807dd7b6ebd766a13 (https://github.com/rails/rails/commit/3fbff7811bc7142e6f4142f807dd7b6ebd766a13#diff-90f6f2b6859f5da65e11631ebcdf3f09)

--- lib/haml_coffee_assets/action_view/resolver.rb
+++ lib/haml_coffee_assets/action_view/resolver.rb
@@ -1,7 +1,13 @@
 require "action_view"
+require 'active_support'
 
 module HamlCoffeeAssets
   module ActionView
+    extend ActiveSupport::Autoload
+    autoload_at "action_view/template/resolver" do
+      autoload :FileSystemResolver  
+    end
+    
     # Custom resolver to prevent Haml Coffee templates from being rendered by
     # Rails for non-HTML formats, since a template name without a MIME type
     # in it would normally be a fallback for all formats.
