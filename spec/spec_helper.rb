fe30c2ec8cd698039187508be4d54f6b21aa6ec8
Sergey Malyshev <job.malyshev@gmail.com>
2014-08-04 17:07:18 +0400

    rspec color_enabled= is deprecated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'rspec'
 require 'haml_coffee_assets'
 
 RSpec.configure do |config|
-  config.color_enabled = true
+  config.color = true
   config.filter_run focus: true
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
